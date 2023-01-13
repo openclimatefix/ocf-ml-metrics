b822ff9535d04df9528cca25b9e481c83b8ba55c
Jacob Bieker <jacob@bieker.tech>
2023-01-13 14:00:54 +0000

    Add basic errors

--- ocf_ml_metrics/metrics/errors.py
+++ ocf_ml_metrics/metrics/errors.py
@@ -1,1 +1,25 @@
 import numpy as np
+
+def common_error_metrics(predictions: np.ndarray, target: np.ndarray, tag: str = ""):
+    """
+    Common error metrics base
+
+    Computes RMSE, NMAE, MAE for
+
+    Args:
+        predictions: Predictions for the given time period
+        target: Ground truth to compare against, or output from baseline model
+        tag: Tag to add to the dictionary keys, if wanted
+
+    Returns:
+        Dictionary of error metrics compute over the given data
+    """
+    error_dict = {}
+
+    error_dict[tag+"nmae"] = np.mean(np.abs(predictions - target))
+    error_dict[tag+"mae"] = np.mean(np.square(predictions - target))
+    error_dict[tag+"rmse"] = np.sqrt(np.mean(np.square(predictions - target)))
+
+    # Now per timestep
+
+    return error_dict
